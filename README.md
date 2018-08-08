0432188feb2791ab0b55797d2b81ec1b147adcaa
Katerina Stanley <katerinastanley77@gmail.com>
2018-08-08 07:47:21 -0400

    edits

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # GifTastic
 
-Dance like nobody's watching!!!
-
-Bollywood
-dance collage
-
-https://www.youtube.com/watch?v=hMxGhHNOkCU&t=2s
-
-build in adobe dimension
-edit in PS
-use for background??
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -76,7 +76,21 @@ $(document).ready(function () {
             // danceImage.attr("src", danceURL);
 
             // $("#dance-view").html(JSON.stringify());
-            // 
+            // // This .on("click") function will trigger the AJAX Call
+            // $("#find-movie").on("click", function (event) {
+
+            //     // event.preventDefault() can be used to prevent an event's default behavior.
+            //     // Here, it prevents the submit button from trying to submit a form when clicked
+            //     event.preventDefault();
+
+            //     // Here we grab the text from the input box
+            //     var movie = $("#movie-input").val();
+
+            //     //Will need API key for Giphy
+            //     // Example queryURL for Giphy API
+            //     var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+            
         });
     });
 
